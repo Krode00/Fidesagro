60e3ac60f0c2b37e90a8d76d75d21bc9cdb998bf
kingkamanda <habeebright97@gmail.com>
2026-09-01 13:42:13 +0100

    initial commit

--- connect/database/rift.py
+++ connect/database/rift.py
@@ -17,7 +17,7 @@
 # annual_income = int(salary) * 12
 # print (annual_income)
 
-# age_difference = max(str(age)) - min(str(age))
+# age_difference = max(age) - min(age)
 # print (27 - 24)
 
 # average_age = (27 + 24) / 2
@@ -26,26 +26,63 @@
 
 
 couple1 = {
-    "name": "oscar",
+    "name": "oscar david",
     "age": 27,
     "city": "kubuwa",
     "occupation": "civil servant",
     "gender": "Male",
     "religion": "Christian",
-    "salary": 300000
+    "salary": 300000,
+    "institution": "university of abuja",
+    "marital_status": "married",
 }
 
 print("Couple 1: ", couple1)
 
 
 couple2 = {
-    "name": "helen",
+    "name": "helen david",
     "age": 24,
     "city": "kubuwa",
     "occupation": "customer and admin rep",
     "gender": "Female",
     "religion": "christian",
-    "salary": 150000
-}
+    "salary": 150000,
+    "institution": "nnamdi azikiwe university",
+    "marital_status": "married"
 
+}
 print("Couple 2: ", couple2)
+
+print ("Couple 1's annual income: ", couple1["salary"] * 12)
+print ("Couple 2's institution: ", couple2["institution"] )
+print ("Couple 1's marital status: ", couple1["marital_status"])
+
+
+oscar_salary = couple1["salary"]
+if oscar_salary > 275000:
+    print("Oscar is a higher earner")
+
+    if couple1["religion"] == couple2["religion"]:
+        print("They share the same religion")
+
+
+couple = [
+    {
+        "name": "oscar david",
+        "occupation": "civil servant",
+        "salary": 300000,
+        "institution": "university of abuja",
+    },
+    {
+        "name": "helen david",
+        "occupation": "customer and admin rep",
+        "salary": 150000,
+        "institution": "nnamdi azikiwe university",
+    }
+]
+for person in couple:
+    print("name:", person["name"])
+    print("occupation:", person["occupation"])
+    print("salary:", person["salary"])
+    print("institution:", person["institution"])
