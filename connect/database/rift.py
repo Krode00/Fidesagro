b83f0e3025e74020e5fe38294048b9c6ebbdb5cb
kingkamanda <habeebright97@gmail.com>
2026-09-03 11:18:24 +0100

    initial commit

--- connect/database/rift.py
+++ connect/database/rift.py
@@ -1,27 +1,27 @@
 
-# name = input("Enter your name ")
-# age = input("how old are you? ")
-# city = input("what city do you live in? ")
-# occupation = input("what is your occupation? ")
-# gender = input("what is your gender? ")
-# hobbies = input("what are your hobbies? ")
-# degree = input("what is your degree? ")
-# field_of_study = input("what is your field of study? ")
-# salary = input("what is your salary? ")
-# marital_status = input("what is your marital status? ")
-# religion = input("what is your religion? ")
+name = input("Enter your name ")
+age = input("how old are you? ")
+city = input("what city do you live in? ")
+occupation = input("what is your occupation? ")
+gender = input("what is your gender? ")
+hobbies = input("what are your hobbies? ")
+degree = input("what is your degree? ")
+field_of_study = input("what is your field of study? ")
+salary = input("what is your salary? ")
+marital_status = input("what is your marital status? ")
+religion = input("what is your religion? ")
 
-# print("my name is " + name + ", I am " + age + " years old, I live in " + city + ", I work as a " + occupation + ", I am " + gender + ", My hobbies include " + hobbies + ", I have a degree in " + degree + ", My field of study is " + field_of_study + ", I earn a salary of " + salary + ", I am " + marital_status + ", My religion is " + religion)
+ print("my name is " + name + ", I am " + age + " years old, I live in " + city + ", I work as a " + occupation + ", I am " + gender + ", My hobbies include " + hobbies + ", I have a degree in " + degree + ", My field of study is " + field_of_study + ", I earn a salary of " + salary + ", I am " + marital_status + ", My religion is " + religion)
 
 
-# annual_income = int(salary) * 12
-# print (annual_income)
+annual_income = int(salary) * 12
+print (annual_income)
 
-# age_difference = max(age) - min(age)
-# print (27 - 24)
+age_difference = max(age) - min(age)
+print (27 - 24)
 
-# average_age = (27 + 24) / 2
-# print (average_age)
+average_age = (27 + 24) / 2
+print (average_age)
 
 
 
